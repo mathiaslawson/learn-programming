0ec6eacba5258e5b19182bda8fd6218ca228cc27
Mathias Prince Lawson <73763892+ingucell@users.noreply.github.com>
2024-04-18 08:14:58 +0000

    landing row 4

--- src/pages/Landing/Row4/index.jsx
+++ src/pages/Landing/Row4/index.jsx
@@ -2,7 +2,7 @@ import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
 import SliderCards from "../Row4/SliderCards";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import { Button } from "reactstrap";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
@@ -21,7 +21,7 @@ function Row4() {
           <div className="text-center" style={{ flex: "1" }}>
             <p
               className="main-header text-center"
-              style={{ fontSize: "5rem", color: "#423F4C" }}
+              style={{ fontSize: "4rem", color: "#423F4C" }}
             >
               Video <br /> Voyage ,Dive <br /> Into Learning <br /> Adventures
             </p>
@@ -88,6 +88,70 @@ function Row4() {
                 <SliderCards />
               </SwiperSlide>
             </Swiper>
+          </div>
+        </div>
+
+        <div className="d-flex justify-content-center">
+          <div
+            style={{
+              width: "75%",
+              backgroundColor: "#A282FD",
+              borderRadius: "15px",
+            }}
+          >
+            <div
+              className="d-flex justify-content-center "
+              style={{
+                marginTop: "8rem",
+
+                color: "white",
+              }}
+            >
+              <div className="mt-5 mx-5">
+                <h1
+                  className="main-header py-3 px-3 text-center"
+                  style={{ fontSize: "4rem" }}
+                >
+                  Infinite learning lab
+                </h1>
+              </div>
+              <div
+                style={{
+                  position: "relative",
+                  top: "-12rem",
+                  left: "7rem",
+                }}
+              >
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/e937/5a13/feab8db7b67400a8b94a2009d1bf576c?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WNeJL2R3l699R5Aizhw12wsE1W-WFPLT2WV0UmNeMLMU1UhkX6g3DuX~05tbyrZEL3dTiv5XbEHY2-Sk3tGCUWdVXHo60CKygu9KXM6I4FXiiE04QyEFkU4spJPQ5cIQ9juK5LNqbTaVDx2wmbqNabUb0SOEsnGph9gQ9o2vPVdgPQGT40ggUXKC~y3TWXNkCEER7jePdrrbqDp1HXapny8Tf50mV4LwwJ79nhedUeaDncpC~FNOiy-4b-ZfatBr6IH9SjckUDRthvZD35g~x4hrnAmMXak3mlZgnlxGfTMLaU75DlVpqnHjlq150pll3ZUJz7cNrfS1xaLY5l9RTA__"
+                  width={"700"}
+                ></img>
+              </div>
+            </div>
+            <div
+              className="text-center text-light mb-5"
+              style={{ marginTop: "-17rem" }}
+            >
+              {" "}
+              <h1 className="main-header">Imagine, Create, Discover</h1>
+              <p className="mt-4 sub-header fs-4 p-4 ">
+                Welcome to the Infinite Learning Lab, where your curiosity leads
+                the way! This special corner of Giggle Learn is your canvas for
+                endless exploration
+              </p>
+              <div>
+                <Button
+                  style={{
+                    backgroundColor: "white",
+                    color: "black",
+                    borderRadius: "35px",
+                  }}
+                  className="px-5 py-3"
+                >
+                  <p className="main-header">Explore Now</p>
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
